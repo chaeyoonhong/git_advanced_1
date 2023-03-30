809ace53db84453384716c39dc0b1f5f0e752743
syt06180 <codbsghd1@gmail.com>
2023-03-30 20:55:03 +0900

    main commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import List
+# Skeleton code for even_list
+def even_list(int_list: List[int]) -> List[int]:
+    pass
+# Skeleton code for sum_of_squares_of_even
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    pass
+def main():
+    int_list =  [58, 6, -22, 91, 33, 49, 44, 63, -79, 12]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+# Boilerplate code
+if __name__ == "__main__":
+    main()
